c9b5f2f0161eb9a86103e1dd022b4589d66a0bb6
gr441_koaal <kulmanakovap@gmail.com>
2026-04-07 16:35:25 +0700

    add checking

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php
-namespace Collect;
+//namespace Collect;
+//
+//require_once 'vendor/autoload.php';
+//
+//$records =
+//    [
+//        'id' => 2135,
+//        'first_name' => 'John',
+//        'last_name' => 'Doe',
+//    ];
+
+
 
 require_once 'vendor/autoload.php';
 
-$records =
-    [
-        'id' => 2135,
-        'first_name' => 'John',
-        'last_name' => 'Doe',
-    ];
+$records = [
+    'id' => 2135,
+    'first_name' => 'John',
+    'last_name' => 'Doe',
+];
+
+$collection = \Collect\collection($records);
+print_r($collection->get());
